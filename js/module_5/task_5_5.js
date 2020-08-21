3d27df8022af6e618b445201a61de9adf53ef54a
Alexander Kholodnyak <kholodnyak.a@gmail.com>
2020-08-21 23:31:48 +0300

    Update task_5_5.js

--- js/module_5/task_5_5.js
+++ js/module_5/task_5_5.js
@@ -4,33 +4,58 @@ class Car {
     return `maxSpeed: ${car.maxSpeed}, speed: ${car.speed}, isOn: ${car.isOn}, distance: ${car.distance}, price: ${car.price}`;
   }
   constructor(car) {
+    this.speed = 0;
+    this._price = car.price;
+    this.isOn = false;
     this.maxSpeed = car.maxSpeed;
+    this.distance = 0;
   }
-  //   get price() {}
-  //   set price(value) {}
-  //   turnOn() {}
-  //   turnOff() {}
-  //   accelerate(value) {}
-  //   decelerate(value) {}
-  //   drive(hours) {}
+  get price() {
+    return this._price;
+  }
+  set price(value) {
+    this._price = value;
+  }
+  turnOn() {
+    this.isOn = true;
+  }
+  turnOff() {
+    this.isOn = false;
+    this.speed = 0;
+  }
+  accelerate(value) {
+    if (value + this.speed <= this.maxSpeed) {
+      this.speed += value;
+    }
+  }
+  decelerate(value) {
+    if (this.speed - value >= 0) {
+      this.speed -= value;
+    }
+  }
+  drive(hours) {
+    if (this.isOn) {
+      this.distance += hours * this.speed;
+    }
+  }
 }
 
 const mustang = new Car({ maxSpeed: 200, price: 2000 });
-// const mustang = new Car({ maxSpeed: 200 });
-// mustang.turnOn();
-// mustang.accelerate(50);
-// mustang.drive(2);
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
 
 console.log(Car.getSpecs(mustang));
 // 'maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000'
 
-// mustang.decelerate(20);
-// mustang.drive(1);
-// mustang.turnOff();
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
 
-// console.log(Car.getSpecs(mustang));
+console.log(Car.getSpecs(mustang));
 // 'maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000'
 
-// console.log(mustang.price); // 2000
-// mustang.price = 4000;
-// console.log(mustang.price); // 4000
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
